5b6850bd2e2af0ecea38b97bdc903ae853567c32
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-06-20 22:03:12 +0700

    create model AdminUser, PaymentMethod, Product

---
+++ app/Models/PaymentMethod.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PaymentMethod extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table = 'payment_method';
+    protected $fillable = [
+        'name',
+        'code',
+        'status',
+        'thumbnail',
+    ];
+}
