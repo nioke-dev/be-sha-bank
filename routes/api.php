2960bd9b268d67464ccf1506d06fb28ee19d4c58
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-06-23 10:29:38 +0700

    creating middleware

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/test', function (Request $request) {
+    return 'success';
+})->middleware('jwt.verify');
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
