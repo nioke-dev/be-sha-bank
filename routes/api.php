2ee798afc0371937f117eedfd021b899fcff5d22
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-06-24 15:18:24 +0700

    top up controller part 1 dan 2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TopUpController;
 use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,4 +13,6 @@ Route::get('/test', function (Request $request) {
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-
+Route::group(['middleware' => 'jwt.verify'], function ($router) {
+    Route::post('top_ups', [TopUpController::class, 'store']);
+});
